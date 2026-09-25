10fa948b31b7c3396c3f2e9d6e44d2d9263306a2
Viernes, Alexander, Jr. M. <alexanderviernesjr.24@gmail.com>
2026-09-26 00:35:57 +0800

    Update lesson-2-control-flow.py

--- module-2-python-basics/lesson-2-control-flow.py
+++ module-2-python-basics/lesson-2-control-flow.py
@@ -1,22 +1,23 @@
 """
 Module 2 — Lesson 2: Control Flow (if / elif / else)
-Student: [your name]
-Date: [date]
+Student: Viernes, Alexander, Jr. M.
+Date: September 24 2026
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
 teaching a friend who's never coded before)
 ============================================
 [write your own explanation here]
+Control flow is how the program decides what to do based on a certain condition. It checks if something is true or false and then chooses the appropriate action. In the python, we use if, elif, and else to handle different situations.
 
 
 ============================================
 KEY VOCABULARY
 ============================================
-- condition:
-- if / elif / else:
-- comparison operator:
-- boolean expression:
+- condition:‎ a statement that the program checks
+- if / elif / else: used to choose between different situations
+- comparison operator: symbols used to compare values, like >, <, or ==
+- boolean expression: an expression that gives either True or ‎False
 (add more as needed)
 
 
@@ -28,6 +29,15 @@ came up with yourself — not copied from class.
 """
 
 # --- your code example goes here ---
+hits = 3
+
+if hits >= 4:
+    print("Amazing game!")
+elif hits >= 2:
+    print("Good job at the plate!")
+else:
+    print("Keep practicing your swing!")
+  
 
 
 """
@@ -36,6 +46,7 @@ A MISTAKE I MADE (or one I want to avoid)
 ============================================
 [what's something confusing or easy to get wrong
 about this topic?]
+One mistake I want to avoid is forgetting to put a colon (:) after if, elif, or else. Sometimes I accidentally type a semicolon (;) because i forgot to press shift on the keyboard its a mistake on my part but it's still one of the things that caused the error in my code
 
 
 ============================================
